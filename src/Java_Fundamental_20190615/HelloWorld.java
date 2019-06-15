ad825ea5f950f86563fc714a2d0b7fad6ea72ab6
Kimmikyung <Kimmikyung@192.168.0.14>
2019-06-15 15:44:50 +0900

    update!!

---
+++ src/Java_Fundamental_20190615/HelloWorld.java
@@ -0,0 +1,12 @@
+package Java_Fundamental_20190615;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Hello mk");
+	System.out.println("study hard");
+	
+	
+	}
+}
